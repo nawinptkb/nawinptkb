d3e025e8b37e8151f52ed9cccd319f9e687c979e
nawinptkb <pantakan.bunleang@gmail.com>
2021-10-17 13:56:12 +0700

    k

--- pyqt_calculator/Pyqt_calculator.py
+++ pyqt_calculator/Pyqt_calculator.py
@@ -1,2 +1,10 @@
-import PyQt5
-import re
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import sys
+
+class UI(QMainWindow):
+        def __init__(self):
+                super(UI,self).__init__()
+
+        #Load the ui file
+        uic.loadUi("Pyqt_calculator.ui", self)
