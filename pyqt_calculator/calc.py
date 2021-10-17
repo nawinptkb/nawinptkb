b7a8214ddd98cf941ad5a420b83adef52ca9e408
nawinptkb <pantakan.bunleang@gmail.com>
2021-10-17 13:51:30 +0700

    cd

---
+++ pyqt_calculator/calc.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Pyqt_calculator.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(585, 392)
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(40, 180, 113, 32))
+        self.pushButton_2 = QPushButton(Dialog)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(180, 180, 113, 32))
+        self.pushButton_3 = QPushButton(Dialog)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(320, 180, 113, 32))
+        self.pushButton_4 = QPushButton(Dialog)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(40, 230, 113, 32))
+        self.pushButton_5 = QPushButton(Dialog)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(180, 230, 113, 32))
+        self.pushButton_6 = QPushButton(Dialog)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setGeometry(QRect(320, 230, 113, 32))
+        self.pushButton_7 = QPushButton(Dialog)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.pushButton_7.setGeometry(QRect(40, 280, 113, 32))
+        self.pushButton_8 = QPushButton(Dialog)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+        self.pushButton_8.setGeometry(QRect(180, 280, 113, 32))
+        self.pushButton_9 = QPushButton(Dialog)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        self.pushButton_9.setGeometry(QRect(320, 280, 113, 32))
+        self.pushButton_10 = QPushButton(Dialog)
+        self.pushButton_10.setObjectName(u"pushButton_10")
+        self.pushButton_10.setGeometry(QRect(450, 280, 113, 32))
+        self.pushButton_11 = QPushButton(Dialog)
+        self.pushButton_11.setObjectName(u"pushButton_11")
+        self.pushButton_11.setGeometry(QRect(450, 340, 113, 32))
+        self.pushButton_12 = QPushButton(Dialog)
+        self.pushButton_12.setObjectName(u"pushButton_12")
+        self.pushButton_12.setGeometry(QRect(450, 230, 113, 32))
+        self.pushButton_13 = QPushButton(Dialog)
+        self.pushButton_13.setObjectName(u"pushButton_13")
+        self.pushButton_13.setGeometry(QRect(450, 180, 113, 32))
+        self.pushButton_14 = QPushButton(Dialog)
+        self.pushButton_14.setObjectName(u"pushButton_14")
+        self.pushButton_14.setGeometry(QRect(450, 130, 113, 32))
+        self.pushButton_15 = QPushButton(Dialog)
+        self.pushButton_15.setObjectName(u"pushButton_15")
+        self.pushButton_15.setGeometry(QRect(320, 130, 113, 32))
+        self.pushButton_16 = QPushButton(Dialog)
+        self.pushButton_16.setObjectName(u"pushButton_16")
+        self.pushButton_16.setGeometry(QRect(180, 130, 113, 32))
+        self.pushButton_17 = QPushButton(Dialog)
+        self.pushButton_17.setObjectName(u"pushButton_17")
+        self.pushButton_17.setGeometry(QRect(40, 130, 113, 32))
+        self.pushButton_18 = QPushButton(Dialog)
+        self.pushButton_18.setObjectName(u"pushButton_18")
+        self.pushButton_18.setGeometry(QRect(40, 330, 113, 32))
+        self.pushButton_19 = QPushButton(Dialog)
+        self.pushButton_19.setObjectName(u"pushButton_19")
+        self.pushButton_19.setGeometry(QRect(180, 330, 113, 32))
+        self.textEdit = QTextEdit(Dialog)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(50, 20, 501, 78))
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"7", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Dialog", u"8", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Dialog", u"9", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Dialog", u"4", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Dialog", u"5", None))
+        self.pushButton_6.setText(QCoreApplication.translate("Dialog", u"6", None))
+        self.pushButton_7.setText(QCoreApplication.translate("Dialog", u"1", None))
+        self.pushButton_8.setText(QCoreApplication.translate("Dialog", u"2", None))
+        self.pushButton_9.setText(QCoreApplication.translate("Dialog", u"3", None))
+        self.pushButton_10.setText(QCoreApplication.translate("Dialog", u"+", None))
+        self.pushButton_11.setText(QCoreApplication.translate("Dialog", u"=", None))
+        self.pushButton_12.setText(QCoreApplication.translate("Dialog", u"-", None))
+        self.pushButton_13.setText(QCoreApplication.translate("Dialog", u"*", None))
+        self.pushButton_14.setText(QCoreApplication.translate("Dialog", u"/", None))
+        self.pushButton_15.setText(QCoreApplication.translate("Dialog", u"C", None))
+        self.pushButton_16.setText(QCoreApplication.translate("Dialog", u"CE", None))
+        self.pushButton_17.setText(QCoreApplication.translate("Dialog", u"x^2", None))
+        self.pushButton_18.setText(QCoreApplication.translate("Dialog", u"0", None))
+        self.pushButton_19.setText(QCoreApplication.translate("Dialog", u".", None))
+    # retranslateUi
+
