8fd927f04aadf4b7f5201d5158349871f025d617
nawinptkb <pantakan.bunleang@gmail.com>
2021-12-16 15:35:36 +0700

    latest change

--- Python_project/Snake game/score_board.py
+++ Python_project/Snake game/score_board.py
@@ -10,7 +10,7 @@ class Score_Board(Turtle):
                 self.score = 0
 
                 #with keyword allows the file not to be closed after being done with the editing; python will save the file to the persistent storage itself.
-                with open('score_data.txt', mode='r') as score_data:
+                with open('/Users/nawinptkb/OneDrive - KMITL/KMITL-NIDA/Financial Engineering Freshmen/Semester 1/Intro To Programming/Github/nawinptkb/nawinptkb/Python_project/Snake game/score_data.txt', mode='r') as score_data:
                         self.high_score = int(score_data.read()) #read the highscore from the score_data.txt and then assign it to the current highscore everytime the game runs
                 
                 self.color('white')
@@ -30,7 +30,7 @@ class Score_Board(Turtle):
         def reset_scoreboard(self):
                 if self.score > self.high_score:
                         self.high_score = self.score #When the current score is higher than the current high score. The high score will be replaced with the current score
-                        with open('score_data.txt',mode='w') as score_data:
+                        with open('/Users/nawinptkb/OneDrive - KMITL/KMITL-NIDA/Financial Engineering Freshmen/Semester 1/Intro To Programming/Github/nawinptkb/nawinptkb/Python_project/Snake game/score_data.txt',mode='w') as score_data:
                                 score_data.write(str(self.high_score)) #Overwrite the old highscore in score_data.txt
 
                 self.score = 0 #Set the current the score back to "0" everytime the reset_scoreboard() method is being called.
